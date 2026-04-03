85cd7b69d8999c1a9e2b4bd2f6bddb44eb4f2c52
Truty424 <86971161+Truty424@users.noreply.github.com>
2026-04-03 13:43:22 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,19 @@
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import padding
+from cryptography.hazmat.primitives.asymmetric import utils
 
 private_key = rsa.generate_private_key( # Noncompliant {{(PrivateKey) RSA}}
     public_exponent=65537,
-    key_size=1024,
+    key_size=2048,
 )
 
-def decrypt(ciphertext):
-    plaintext = private_key.decrypt(
-        ciphertext,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA384()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
-    return plaintext
+message = b"A message I want to sign"
+signature = private_key.sign(
+    message,
+    padding.PSS(
+        mgf=padding.MGF1(hashes.SHA256()),
+        salt_length=padding.PSS.MAX_LENGTH
+    ),
+    utils.Prehashed(hashes.SHA384())
+)
