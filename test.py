03f1e78451011cc9ef59ff1a19a630c32a9d002e
Truty424 <86971161+Truty424@users.noreply.github.com>
2026-04-03 13:16:09 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,25 +1,19 @@
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.keywrap import aes_key_wrap, aes_key_unwrap
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 
-def aes_key_wrap_example():
-    # Generate a key to wrap
-    key_to_wrap = b'Sixteen byte key'
+private_key = rsa.generate_private_key( # Noncompliant {{(PrivateKey) RSA}}
+    public_exponent=65537,
+    key_size=1024,
+)
 
-    # Generate wrapping key (must be 128, 192, or 256 bits long)
-    wrapping_key = b'ABCDEFGHIJKLMNOP'
-
-    # Wrap the key
-    wrapped_key = aes_key_wrap(wrapping_key, key_to_wrap, default_backend()) # Noncompliant {{(KeyWrap) AES128}}
-
-    print("Wrapped Key:", wrapped_key.hex())
-
-    # Unwrap the key
-    unwrapped_key = aes_key_unwrap(wrapping_key, wrapped_key, default_backend())
-
-    print("Unwrapped Key:", unwrapped_key.hex())
-
-    # Ensure that the unwrapped key matches the original key
-    assert unwrapped_key == key_to_wrap
-
-if __name__ == "__main__":
-    aes_key_wrap_example()
+def decrypt(ciphertext):
+    plaintext = private_key.decrypt(
+        ciphertext,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA384()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+    )
+    return plaintext
