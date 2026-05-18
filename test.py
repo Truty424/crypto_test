04795b72e5445d885363a71348913830d401c456
Truty424 <86971161+Truty424@users.noreply.github.com>
2026-05-18 12:46:53 +0200

    sha1

--- test.py
+++ test.py
@@ -1,15 +1,11 @@
-import os
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
-salt = os.urandom(16)
-info = b"hkdf-example"
-
-hkdf = HKDF( # Noncompliant {{(KeyDerivationFunction) HKDF-SHA256}}
-    algorithm=hashes.SHA256(),
-    length=32,
-    salt=salt,
-    info=info,
-)
-
-key = hkdf.derive(b"input key")
+static MessageDigest createMessageDigest() {
+    try {
+        return MessageDigest.getInstance("SHA-1");
+    } catch (NoSuchAlgorithmException e) {
+        throw new RuntimeException(
+                "Cannot instantiate the message digest algorithm " + "SHA-1", e);
+    }
+}
