e7f324c843a330510f821617e4da871d551b463b
Truty424 <86971161+Truty424@users.noreply.github.com>
2026-05-18 13:01:33 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,18 @@
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-static byte[] createDigest(byte[] input) throws NoSuchAlgorithmException {
-    MessageDigest messageDigest = MessageDigest.getInstance("SHA-1");
-    return messageDigest.digest(input);
+public class Main {
+
+    static byte[] createDigest(byte[] input) throws NoSuchAlgorithmException {
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-1");
+        return messageDigest.digest(input);
+    }
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        byte[] result = createDigest("tekst".getBytes());
+
+        for (byte b : result) {
+            System.out.printf("%02x", b);
+        }
+    }
 }
